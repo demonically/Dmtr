375830302ba169ff6ce11379ec32e8d6a8321e19
DimkaKrasikov <evildemon1579@gmail.com>
2024-02-27 19:16:58 +0300

    123

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <iostream>
+
 int main()
 {
-  printf("Hello world\n");
-  return 0;
+    for(int i {0};i!=10000;i++)
+    {
+        std::cout << "int: " << i << std::endl;
+    }
+    printf("pizdecs");
+    return 0;
 }
